a3a54c9e3e5ad43621ab5316aa05a43890fb9847
David Konsumer <konsumer@jetboystudio.com>
2021-01-12 19:42:59 -0800

    no stream, use buffer

--- api/[id].js
+++ api/[id].js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default async (req, res) => {
   const { id } = req.query
   const URL = `http://8.240.242.124/${id.substr(0, 2)}/${id.substr(2,2)}/${id}`
-  const r = await axios.get(URL, { responseType: 'stream', headers: { 'Host': 'video.parler.com' } })
-  res.setHeader('Content-Disposition', `attachment; filename="${id}.mp4"`)
-  r.data.pipe(res)
+  const r = await axios.get(URL, { responseType: 'arraybuffer', headers: { 'Host': 'video.parler.com' } })
+  res.send(r.data)
 }
