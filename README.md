5d4e8532be36b3f769283f219e3a49a01221a2c5
David Konsumer <konsumer@jetboystudio.com>
2021-01-12 19:34:02 -0800

    intitial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Dowmnload Parler videos after the takedown, like this:
+
+```
+https://parlervid.vercel.app/VIDEOID
+```
+
+Where `VIDEOID` is the id of the video you want.
---
+++ api/[id].js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default async (req, res) => {
+  const { id } = req.query
+  const URL = `http://8.240.242.124/${id.substr(0, 2)}/${id.substr(2,2)}/${id}`
+  const r = await axios.get(URL, { responseType: 'stream', headers: { 'Host': 'video.parler.com' } })
+  res.setHeader('Content-Disposition', `attachment; filename="${id}.mp4"`)
+  r.data.pipe(res)
+}
