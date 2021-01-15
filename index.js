0c1948fc5cf614b2dc492754e3674dc439275a93
David Konsumer <konsumer@jetboystudio.com>
2021-01-15 02:43:05 -0800

    another mirror, youtube forward

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require('express')
 const request = require('request')
-const mirror1 = require('./mirror1.json')
-const mirror2 = require('./mirror2.json')
+const ytdl = require('ytdl-core')
+
+const mirrors = [
+  require('./mirror1.json'), // https://gofile.io/d/cY5TtS
+  require('./mirror2.json'), // https://www.tommycarstensen.com/terrorism/
+  require('./mirror3.json')  // https://thepatr10t.github.io/yall-Qaeda
+]
 
 const PORT = process.env.PORT || 5000
 
 express()
+  // redirect to raw MP4 URL for youtube video
+  .get('/yt/:id', async (req, res) => {
+    const { id } = req.params
+    const info = await ytdl.getInfo(`http://www.youtube.com/watch?v=${id}`)
+    res.redirect(301, info.formats.filter(v => v.container == 'mp4' && v.qualityLabel == '480p60').pop().url)
+  })
+
+  // find the best method of downloading the parler video
   .get('/:id', (req, res) => {
     const { id } = req.params
     if (!id) {
       throw new Error('No id.')
     }
 
-    if (mirror1[id]) {
-      console.log(`found ${id} in mirror1`)
-      return res.redirect(mirror1[id])
+    for (mirror of mirrors) {
+      if (mirror[id]){
+        return res.redirect(mirror[id])
+      }
     }
 
-    if (mirror2[id]) {
-      console.log(`found ${id} in mirror2`)
-      return res.redirect(mirror2[id])
-    }
-
-    console.log(`${id} not found in mirror, trying IP`)
+    console.log(`${id} not found in mirror, trying IP trick`)
 
     request({
       url: `http://8.240.242.124/${id.substr(0, 2)}/${id.substr(2, 2)}/${id}`,
@@ -31,7 +40,10 @@ express()
       }
     }).pipe(res)
   })
+  
+  // show the map
   .get('/', (req, res) => {
     res.sendFile('map.html', { root: __dirname })
   })
+  
   .listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
